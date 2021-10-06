3aa53e2f523f3c793e6d1e2ab2a25745d3c52fff
IAM78 <91953698+IAM78@users.noreply.github.com>
2021-10-06 03:34:06 -0700

    Add files via upload

---
+++ bmi.py
@@ -0,0 +1,19 @@
+print ('please enter your weight by Kg and height by metre :')
+print('weight : ')
+w=float(input())
+print('height : ')
+h=float(input())
+b=round((w/h**2),1)
+print('\n your bmi is ',b)
+
+
+if b<18.5:
+    print('you are under weight')
+elif b>=18.5 and b<=24.9:
+    print('you are normal')
+elif b>24.9 and b <=29.9:
+    print('you are over weight')
+elif b>29.9 and b <=34.9:
+    print('you are Obese')
+elif b>34.9 :
+    print('you are extremely Obese ')
